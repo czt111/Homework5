5fe1ae780f5b273616054913c0703ab2f2f7253c
Zhitong Chen <148817346+czt111@users.noreply.github.com>
2023-11-12 20:02:30 -0500

    Add files via upload

--- SpellChecker.java
+++ SpellChecker.java
@@ -4,12 +4,66 @@ import java.util.ArrayList;
 
 public class SpellChecker {
     private WordRecommender recommender;
-
     public SpellChecker(String dictionaryFile) {
         this.recommender = new WordRecommender(dictionaryFile);
     }
 
     public void start(String fileToCheck) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Input dictionary file
+        String dictionaryFileName = "";
+        boolean validDictionary = false;
+
+        System.out.println("Please enter the name of a file to use as a dictionary.");
+
+        while (!validDictionary) {
+            dictionaryFileName = scanner.nextLine();
+
+            try {
+                File file = new File(dictionaryFileName);
+
+                if (file.exists()) {
+                    System.out.println("Using the dictionary at '" + dictionaryFileName + "'.");
+                    validDictionary = true;
+                } else {
+                    System.out.println("There was an error in opening that file.");
+                    System.out.println("Please enter the name of a file to use as a dictionary.");
+                }
+            } catch (Exception e) {
+                System.out.println("There was an error in opening that file.");
+                System.out.println("Please enter the name of a file to use as a dictionary.");
+            }
+        }
+
+        // Input file for spell check
+        String spellCheckFileName = "";
+        boolean validFileForSpellCheck = false;
+
+        System.out.println("\nPlease enter the name of a file to be spell checked.");
+
+        while (!validFileForSpellCheck) {
+            spellCheckFileName = scanner.nextLine();
+
+            try {
+                File file = new File(spellCheckFileName);
+
+                if (file.exists()) {
+                    System.out.println("Spell checking for '" + spellCheckFileName + "' will be output in '" + spellCheckFileName.replace(".txt", "_chk.txt") + "'.");
+                    validFileForSpellCheck = true;
+                } else {
+                    System.out.println("There was an error in opening that file.");
+                    System.out.println("Please enter the name of a file to be spell checked.");
+                }
+            } catch (Exception e) {
+                System.out.println("There was an error in opening that file.");
+                System.out.println("Please enter the name of a file to be spell checked.");
+            }
+        }
+
+        scanner.close();
+
         File spellCheckFile = new File(fileToCheck);
         try (Scanner scanner = new Scanner(spellCheckFile)) {
             while (scanner.hasNext()) {
--- SpellCheckerRunner.java
+++ SpellCheckerRunner.java
@@ -1,61 +1,8 @@
-import java.io.File;
-import java.util.Scanner;
-
+// Do not modify this file whatsoever.
 public class SpellCheckerRunner {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Input dictionary file
-        String dictionaryFileName = "";
-        boolean validDictionary = false;
-
-        System.out.println("Please enter the name of a file to use as a dictionary.");
-
-        while (!validDictionary) {
-            dictionaryFileName = scanner.nextLine();
-
-            try {
-                File file = new File(dictionaryFileName);
-
-                if (file.exists()) {
-                    System.out.println("Using the dictionary at '" + dictionaryFileName + "'.");
-                    validDictionary = true;
-                } else {
-                    System.out.println("There was an error in opening that file.");
-                    System.out.println("Please enter the name of a file to use as a dictionary.");
-                }
-            } catch (Exception e) {
-                System.out.println("There was an error in opening that file.");
-                System.out.println("Please enter the name of a file to use as a dictionary.");
-            }
-        }
-
-        // Input file for spell check
-        String spellCheckFileName = "";
-        boolean validFileForSpellCheck = false;
-
-        System.out.println("\nPlease enter the name of a file to be spell checked.");
-
-        while (!validFileForSpellCheck) {
-            spellCheckFileName = scanner.nextLine();
-
-            try {
-                File file = new File(spellCheckFileName);
-
-                if (file.exists()) {
-                    System.out.println("Spell checking for '" + spellCheckFileName + "' will be output in '" + spellCheckFileName.replace(".txt", "_chk.txt") + "'.");
-                    validFileForSpellCheck = true;
-                } else {
-                    System.out.println("There was an error in opening that file.");
-                    System.out.println("Please enter the name of a file to be spell checked.");
-                }
-            } catch (Exception e) {
-                System.out.println("There was an error in opening that file.");
-                System.out.println("Please enter the name of a file to be spell checked.");
-            }
-        }
-
-        scanner.close();
+        SpellChecker checker = new SpellChecker();
+        checker.start();
     }
 }
 
